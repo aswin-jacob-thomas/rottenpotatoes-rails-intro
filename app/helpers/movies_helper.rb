e9ea883d9dd6fd8b7af75c7d36355b677398c5db
EC2 Default User <ec2-user@ip-172-31-29-121.us-east-2.compute.internal>
2019-10-03 21:57:40 +0000

    Changed the movies helper function sortable

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,7 +6,7 @@ module MoviesHelper
   
   def sortable(column)
     title = column.titleize
-    direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
+    direction = column == sort_attribute && direction_attribute == "asc" ? "desc" : "asc"
     link_to title, movies_path(:sort=>column, :direction=>direction), :class=>"links", :id=>column+"_header"
   end  
   
